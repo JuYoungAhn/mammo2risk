09e8e07a84dcb2227b4bb2c32a6f69bda66537cb
JuYoungAhn <juyoung.ahn@snu.ac.kr>
2019-12-30 17:30:38 +0900

    191230

--- tests/noxfile.py
+++
@@ -1,7 +0,0 @@
-import nox
-
-@nox.session(python="3.6")
-def test(session):
-  session.install("-e", "../")
-  # session.run("mammo2risk", "--o", ".")
-  session.run("mammo2risk", "--o", ".", "--d", "../docs/samples/")
--- tests/samples/noxfile.py
+++
@@ -1,8 +0,0 @@
-import nox
-
-@nox.session(python="3.6")
-def test(session):
-  session.install("-e", "../../")
-  # session.run("mammo2risk", "w", "../downloads/weights/", "o", ".")
-  session.run("mammo2risk", "--r")
-  # session.run("mammo2risk", "--o", ".", "--d", "../docs/samples/")
